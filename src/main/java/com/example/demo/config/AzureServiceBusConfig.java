2e3460fcfd66d32c3631f9c5b15b6826ed1c4dfc
Risni Dheerasekara <risni.dheerasekara@visma.com>
2026-07-10 13:14:51 +0530

    message consumer bean created

--- src/main/java/com/example/demo/config/AzureServiceBusConfig.java
+++ src/main/java/com/example/demo/config/AzureServiceBusConfig.java
@@ -1,15 +1,20 @@
 package com.example.demo.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.azure.messaging.servicebus.ServiceBusClientBuilder;
+import com.azure.messaging.servicebus.ServiceBusProcessorClient;
 import com.azure.messaging.servicebus.ServiceBusSenderClient;
 
 @Configuration
 public class AzureServiceBusConfig {
-    @Bean
+	private static final Logger logger = LoggerFactory.getLogger(AzureServiceBusConfig.class);
+
+	@Bean
 	ServiceBusSenderClient senderClient(
 			@Value("${azure.servicebus.connection-string}") String connectionString,
 			@Value("${azure.servicebus.topic-name}") String topicName) {
@@ -20,4 +25,29 @@ public class AzureServiceBusConfig {
 				.topicName(topicName)
 				.buildClient();
 	}
+
+	@Bean(destroyMethod = "close")
+	ServiceBusProcessorClient processorClient(
+			@Value("${azure.servicebus.connection-string}") String connectionString,
+			@Value("${azure.servicebus.topic-name}") String topicName,
+			@Value("${azure.servicebus.subscription-name}") String subscriptionName) {
+
+		ServiceBusProcessorClient processorClient = new ServiceBusClientBuilder()
+				.connectionString(connectionString)
+				.sessionProcessor()
+				.topicName(topicName)
+				.subscriptionName(subscriptionName)
+				.maxConcurrentSessions(2)
+				.processMessage(context -> {
+					logger.info("received the notification subscription message:{}", context.getMessage().getBody());
+				})
+				.processError(context -> {
+					logger.error("Error occurred while processing message: {}", context.getException().getMessage());
+				})
+				.buildProcessorClient();
+
+		processorClient.start();
+		return processorClient;
+	}
+
 }
